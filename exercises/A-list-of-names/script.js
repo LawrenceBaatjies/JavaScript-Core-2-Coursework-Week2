224d6be850cc4b654153dbc540bd65777602db68
LawrenceBaatjies <lawrencebaatjies47@gmail.com>
2021-03-05 22:21:16 +0200

    A-liat-of-names Competed
    Exercise 5 Completed.

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,18 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+};
+
+for (let i = 0; i < arrayOfPeople.length; i++) {
+  
+  let h1El = document.createElement("h1");    //Created "h1" tag and appended it to the "div"
+  content.appendChild(h1El);
+
+  
+  let h2Els = document.createElement("h2");   //Created "h2" tag and appended it to the "div"
+  content.appendChild(h2Els);
+
+  h1El.innerText = people[i].name;
+  h2Els.innerText = people[i].job;
 }
 
 let people = [
