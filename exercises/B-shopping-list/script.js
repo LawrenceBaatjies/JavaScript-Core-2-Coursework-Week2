66842a0e159787dc89b46c8491cb8432e0ee0039
LawrenceBaatjies <lawrencebaatjies47@gmail.com>
2021-03-05 23:00:01 +0200

    B-Shopping-list
    Exersice 2 Completed

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,7 +1,18 @@
 function shoppingList(arrayOfPeople) {
-  // Write your code here...
+
+  const divEl = document.getElementById("content");
+  const ulEl = document.createElement("ul");
+ 
+    for (let i = 0; i < arrayOfPeople.length; i++) {
+
+    const liEls = document.createElement("li");
+    liEls.innerHTML = arrayOfPeople[i];
+    ulEl.appendChild(liEls);
+    divEl.appendChild(ulEl);
+  }
+  console.log(divEl);
 }
 
-let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
+let shopping = ["Milk", "Bread", "Eggs", "A Toy Car", "Chips", "Sugar", "Coffee"];
 
 shoppingList(shopping);
