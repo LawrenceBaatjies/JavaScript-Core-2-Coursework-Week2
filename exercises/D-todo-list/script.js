0f6df3de5f67ce09ca6b7dba39a47afd3d74446f
LawrenceBaatjies <lawrencebaatjies47@gmail.com>
2021-03-09 20:53:21 +0200

    D-toDo List-completed
    Exercise -D -Completed

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,46 @@
 function todoList(todos) {
   // Write your code here...
-}
+
+  let body = document.querySelector("body");
+  let h1Tag = document.createElement("h1");
+  h1Tag.innerText = "Todo List";
+  content.appendChild(h1Tag);
+  // Creates ul element inside body
+  let ulTag = document.createElement("ul");
+  body.appendChild(ulTag);
+
+  for (let job of todos){
+    
+    let liTag = document.createElement("li");
+    liTag.innerText = job.todo;
+    ulTag.appendChild(liTag);
+
+    
+    liTag.addEventListener("click", lineThrough);
+
+    function lineThrough() {
+      
+      if (liTag.style.textDecoration === "line-through") {
+        liTag.style.textDecoration = "none";
+       
+      } else {
+        liTag.style.textDecoration = "line-through";
+      }
+    }
+  }
+
+  // * All of your HTML should go inside the `<div>` with the id **"content"**.
+  content.appendChild("ulTag");
+};
+
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
+  {todo: "Help With Supper"}, 
+  {todo: "Bath the Boys"}, 
   {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  {todo: "Spend time with wife"},
+  {todo: "Go sleep to get up for work"},
+  
 ];
 
 todoList(todos);
