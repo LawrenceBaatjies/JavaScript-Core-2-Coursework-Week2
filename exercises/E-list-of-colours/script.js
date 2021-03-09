33df4f45d6b0e3bdd12e4e55aee0036cda238ea1
LawrenceBaatjies <lawrencebaatjies47@gmail.com>
2021-03-09 21:03:43 +0200

    E-List-of-Colors completee
    Exercise - E- Completed

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,7 +1,34 @@
 function listOfColours(colours) {
   // Write your code here...
-}
 
-const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
+  let selectTag = document.createElement("select");
+  content.appendChild(selectTag);
+
+ 
+  let pTag = document.createElement("p");
+  content.appendChild(pTag);
+
+  
+  for (colour of colours) {
+   
+    let optionTag = document.createElement("option");
+    optionTag.value = colour;
+    optionTag.innerText = colour;
+    selectTag.appendChild(optionTag);
+
+    
+  selectTag.addEventListener("click", colourSelection);
+
+    function colourSelection() {
+      pTag.innerText = `You have selected: ${selectTag.value}`;
+      pTag.style.color = selectTag.value;
+    }
+  }
+  
+  content.appendChild(selectTag);
+};
+
+
+const colours = ["red", "blue", "green", "yellow", "grey", "black"];
 
 listOfColours(colours);
